315a9d10921fc8dd93c7229a13b93a53510030f8
JoyM268 <125041935+JoyM268@users.noreply.github.com>
2024-06-09 11:01:24 +0530

    Add File

---
+++ Unit - 2/Basic Traversal and Search Techniques/C implementation/BFSConnected.c
@@ -0,0 +1,97 @@
+//Program to check if graph is connected or not using bfs
+#include<stdio.h>
+#define MAX 100
+
+int count = 0, queue[MAX], front = 0, rear = -1;
+void insertQueue(int element)
+{
+    queue[++rear] = element;
+}
+
+void deleteQueue()
+{
+    queue[front++];
+    if(front > rear)
+    {
+        front = 0;
+        rear = -1;
+    }
+}
+
+void readMatrix(int matrix[][MAX], int n)
+{
+    int i, j;
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+}
+
+void markUnvisited(int visitedNodes[], int nodeCount)
+{
+    int i;
+    for(i = 0; i < nodeCount; i++)
+    {
+        visitedNodes[i] = 0;
+    }
+}
+
+void bfs(int v, int graph[][MAX], int visitedNodes[], int nodeCount)
+{
+    count++;
+    visitedNodes[v] = count;
+    insertQueue(v);
+    while(front <= rear)
+    {
+        int frontElement = queue[front], w;
+        for(w = 0; w < nodeCount; w++)
+        {
+            if(graph[frontElement][w] == 1 && visitedNodes[w] == 0)
+            {
+                count++;
+                visitedNodes[w] = count;
+                insertQueue(w);
+            }
+        }
+        deleteQueue();
+    }
+}
+
+int isConnected(int graph[][MAX], int visitedNodes[], int nodeCount)
+{
+    int v;
+    for(v = 0; v < nodeCount; v++)
+    {
+        if(visitedNodes[v] == 0)
+        {
+            bfs(v, graph, visitedNodes, nodeCount);
+            if(count == nodeCount)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
+
+int main()
+{
+    int nodeCount, graph[MAX][MAX], visitedNodes[MAX];
+    printf("Enter the number of nodes: ");
+    scanf("%d", &nodeCount);
+    printf("Enter the adjacency matrix:\n");
+    readMatrix(graph, nodeCount);
+    markUnvisited(visitedNodes, nodeCount);
+    if(isConnected(graph, visitedNodes, nodeCount))
+    {
+        printf("The graph is connected\n");
+    }
+    else
+    {
+        printf("The graph is not connected\n");
+    }
+    return 0;
+}
